58b47c5eb9d2845f8faed6ff7d55907a88e7b8a3
Garrett Leber <garrett@leberra.com>
2025-05-01 12:02:21 -0400

    fix: add comment about why we're using direct ip for proxmox endpoint

--- proxmox/dev/network/providers.tf
+++ proxmox/dev/network/providers.tf
@@ -1,4 +1,6 @@
 provider "proxmox" {
+  # proxmox.imkumpy.in is reverse proxied through another host which causes issues
+  # when this provider tries to SSH, so we use the direct IP address here.
   endpoint = "https://192.168.0.100:8006/"
 
   api_token = var.proxmox_api_token
